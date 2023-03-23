4136f194c8e0e3d24f0cc8e42e6db7f32b7e5db8
NguyenHaiLong2002 <hailong10082002@gmail.com>
2023-03-23 12:34:35 +0700

    Add project files.

---
+++ NguyenHaiLong_2011068860/Migrations/202303201334273_AddNameColumnToApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿namespace NguyenHaiLong_2011068860.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameColumnToApplicationUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String(maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
---
+++ NguyenHaiLong_2011068860/Models/Attendance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenHaiLong_2011068860.Models
+{
+    public class Attendance : Controller
+    {
+        // GET: Attendance
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
